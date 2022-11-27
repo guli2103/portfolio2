6a0f0729863930da560eeb8dc075bf23555317a0
guli2103 <gulbahorcoder@gmail.com>
2022-11-27 22:30:15 +0500

    portfolio

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,13 +1,29 @@
-/*===== MENU SHOW Y HIDDEN =====*/
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
 
-// SHOW
-
-
-// HIDDEN
+showMenu('nav-toggle','nav-menu')
 
 
 /*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav_link')
 
+function linkAction(){
+    navLink.forEach(n => n.classList.remove('activate'))
+    this.classList.add('active')
+
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction))
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
