19dfe1ba257e4e016aaa78b292529a33b0cde18d
guli2103 <gulbahorcoder@gmail.com>
2022-11-28 15:13:01 +0500

    my portfolio

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -17,7 +17,7 @@ showMenu('nav-toggle','nav-menu')
 const navLink = document.querySelectorAll('.nav_link')
 
 function linkAction(){
-    navLink.forEach(n => n.classList.remove('activate'))
+    navLink.forEach(n => n.classList.remove('active'))
     this.classList.add('active')
 
     const navMenu = document.getElementById('nav-menu')
@@ -26,4 +26,35 @@ function linkAction(){
 
 navLink.forEach(n => n.addEventListener('click',linkAction))
 
-/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+/*===== SCROLL SECTIONS ACTIVE =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true,
+})
+
+/* SCROLL ABOUT */
+sr.reveal('.home_title',{})
+sr.reveal('.button',{delay: 200})
+sr.reveal('.home_img',{delay: 400})
+sr.reveal('.home_social-icon',{interval: 200})
+
+/*SCROLL ABOUT*/
+sr.reveal('.about_img',{})
+sr.reveal('.about_subtitle',{delay: 200})
+sr.reveal('.about_text',{delay: 400})
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills_subtitle',{})
+sr.reveal('.skills_text',{delay: 200})
+sr.reveal('.skills_data',{interval: 200})
+sr.reveal('.skills_img',{delay: 400})
+
+/*SCROLL WORK*/
+sr.reveal('.work_img',{interval: 200})
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact_input',{interval: 200})
+
+
